5f8d471b777c66f3e5627bc2c76e717174d0fce7
dmitriy <dmitriy.ardelyan@gmail.com>
2022-02-10 14:46:06 +0200

    method added in feature branch

--- src/main/java/test/dmytro/ard/Feature20993.java
+++ src/main/java/test/dmytro/ard/Feature20993.java
@@ -3,4 +3,8 @@ package test.dmytro.ard;
 public class Feature20993 {
 
     private String stringAddedInFeatureBrach;
+
+    public String getString(){
+        return stringAddedInFeatureBrach;
+    }
 }
