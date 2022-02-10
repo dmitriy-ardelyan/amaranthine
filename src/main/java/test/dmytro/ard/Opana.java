d1a6d21d2b6a8b160e0980f8e2193773446d07f5
dmitriy <dmitriy.ardelyan@gmail.com>
2022-02-10 14:24:56 +0200

    feature 12992 trololo started

--- src/main/java/test/dmytro/ard/Opana.java
+++ src/main/java/test/dmytro/ard/Opana.java
@@ -1,6 +1,11 @@
 package test.dmytro.ard;
 
 public class Opana {
+    public static String opana1 = "Opana1";
+
+    public void trololo(){
+        System.out.println("Trololo");
+    }
 
     private void opana1(){
         System.out.println("opana");
